20897856728dd27c3b65ff651702bd521f98b1c5
Declan Briggs <dtbriggs427@gmail.com>
2022-09-20 18:49:41 +1000

    Set up POST request form w/ error handling

--- app.py
+++ app.py
@@ -1,12 +1,33 @@
-from flask import Flask, render_template
+from crypt import methods
+from math import fabs
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+PREDICT_ENABLED = None
 
 
-@app.route('/welcome')
+@app.route('/')
 def welcome():
-    return render_template('index.html')
+    PREDICT_ENABLED = False
+    return render_template('index.html', predict=PREDICT_ENABLED)
+
+
+@app.errorhandler(404)
+def pageNotFound():
+    return render_template('error.html'), 404
+
+
+@app.route('/predict', methods=['POST', 'GET'])
+def predict():
+    if request.method == 'POST':
+        PREDICT_ENABLED = True
+        model = request.form['model']
+
+        return render_template('index.html', predict=PREDICT_ENABLED, model=model) 
+
+    if request.method == 'GET':
+        return render_template('error.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
