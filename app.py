fc034096f23a970ed6b6c54de04dbb941120cb76
Declan Briggs <dtbriggs427@gmail.com>
2022-09-27 18:29:50 +1000

    Redid form inputs and split datetime function

--- app.py
+++ app.py
@@ -5,6 +5,24 @@ app = Flask(__name__)
 PREDICT_ENABLED = None
 
 
+def format_datetime(input):
+
+    split_datetime = input.split('T')
+    raw_date = split_datetime[0]
+    raw_time = split_datetime[1]
+
+    split_date = raw_date.split('-')
+    split_time = raw_time.split(':')
+
+    print(split_date)
+    print(split_time)
+
+def predict_money(year, month, day, hour):
+    # Add prediction logic here
+
+    return
+
+
 @app.route('/')
 def welcome():
     PREDICT_ENABLED = False
@@ -20,27 +38,27 @@ def pageNotFound():
 def predict():
     if request.method == 'POST':
         PREDICT_ENABLED = True
-        if request.form['model'] == 'default':
-            error = 'No model selected. Please select a model.'
-            return render_template('error.html', error=error)
-        # Insert logic to get result from model
-
-        # Receive date from template
-
-        predict_money()
+        # Receive datetime from template
+        predict_list = request.form.to_dict()
+        predict_list = list(predict_list.values())
+        print(request.form['input'])
+        format_datetime(request.form['input'])
 
         result = None # will be changed...
-        return render_template('index.html', predict=PREDICT_ENABLED, result=result, model=request.method['model']) 
+        return render_template('index.html', predict=PREDICT_ENABLED, result=result) 
 
     if request.method == 'GET':
         return render_template('error.html')
 
 
+<<<<<<< HEAD
 def predict_money(hour, day, month, year):
     hour = 0
     
 
 
+=======
+>>>>>>> 279a918 (Redid form inputs and split datetime function)
 if __name__ == '__main__':
     lstm_model = pickle.load(open('pickles/RNN_LSTM_SIMPLE.pkl', 'rb'))
     gru_model = pickle.load(open('pickles/RNN_GRU_SIMPLE.pkl', 'rb'))
